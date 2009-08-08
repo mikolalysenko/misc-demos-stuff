3771935f0757e9838678549d0b40bc60a1ff4e66
mikolalysenko <mikolalysenko@74b700b4-4ff9-11de-958d-853360c022fe>
2009-08-08 21:21:10 +0000

    Adding tetris stuff

---
+++ trunk/Tetris/src/project/game.h
@@ -0,0 +1,37 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <Eigen/Core>
+#include <Eigen/Geometry>
+
+
+//Game configuration variables
+namespace Game
+{
+	//Screen resolution
+	extern bool fullscreen;
+	extern int XRes, YRes;
+	
+	//Camera variables
+	extern float fov, z_near, z_far;
+	
+	//Time quantum
+	extern float delta_t;
+	
+
+	//Initialization function
+	void init();
+	
+	//Update callback
+	void update(float deltat);
+	
+	//Rendering
+	void draw();
+	
+	//HUD stuff / overlays
+	void overlays();
+};
+
+#endif
+
+
